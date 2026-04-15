d2a3e83cd1a37fe785197daa268a2568e3a0a7cf
SamsonGulma <sams0ngultek@gmail.com>
2026-04-15 23:44:28 +0300

    add: vite and react

---
+++ devops-intro/src/App.jsx
@@ -0,0 +1,130 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Introduction to DevOps</h1>
+          <p>
+            Bridging the Gap Between <code>Development</code> and <code>IT Operations</code> 
+ 
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count ? 5 : `not only ${ count + 1}`)}
+        >
+         (Click:👉) Number of Team members are {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>We demonstrate the CI/CD on the following the stacks</h2>
+          <p>React / Vite</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                For more <br /> Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                For more <br /> Explore React
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+          </svg>
+          <h2>Brought to you by</h2>
+          <p>Your best presenters about DevOps</p>
+          <ul>
+            <li>
+              <a href="" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                </svg>
+               <p>Samson Gulma</p>
+              </a>
+            </li>
+            <li>
+              <a href="" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                </svg>
+               <p> Amanuel Bantidagn </p>
+              </a>
+            </li>
+            <li>
+              <a href="" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                </svg>
+               <p> Biruk Gabisa</p>
+              </a>
+            </li>
+            <li>
+              <a href="" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+
+                </svg>
+               <p> Solomon Dugasa</p>
+              </a>
+            </li>
+            <li>
+              <a href="" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  
+                </svg>
+                <p>Yonas Teshome</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
